3327302528f6220df9a0686e61eaf18d50bac88d
Aleksander <aleksanderzak97@gmail.com>
2025-03-06 14:27:08 +0100

    Implementacja Nawigacji między okienkami

---
+++ Wulkanizacja.Desktop/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using Wulkanizacja.User.Services;
+using Wulkanizacja.User.ViewModels;
+
+namespace Wulkanizacja.User;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public new static App Current => (App)Application.Current;
+    public ServiceProvider ServiceProvider { get; private set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        ServiceProvider = services.BuildServiceProvider();
+
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private void ConfigureServices(ServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+        services.AddSingleton<MainWindowViewModel>();
+
+        services.AddTransient<LoginViewModel>();
+        services.AddTransient<GeneralViewModel>();
+        services.AddTransient<ContentControlViewModel>();
+
+        services.AddSingleton<INavigationService, NavigationService>();
+    }
+}
+
---
+++ Wulkanizacja.Desktop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Wulkanizacja.User.Services;
+using Wulkanizacja.User.Views;
+
+namespace Wulkanizacja.User.ViewModels
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        private readonly INavigationService _navigationService;
+
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public event EventHandler LoggedIn;
+
+        public LoginViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            LoginCommand = new RelayCommand(ExecuteLogin);
+        }
+
+        private void ExecuteLogin(object parameter)
+        {
+            _navigationService.NavigateTo("General");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
